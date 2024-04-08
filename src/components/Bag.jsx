b9d57b10c9cf0864d8229dbf01dbdc6645debbcb
Ashrita29 <ashumahapatra2903@gmail.com>
2024-04-08 10:01:35 +0530

    updated

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -46,7 +46,9 @@ export const Bag = () => {
             <p className="dark:text-white ">Your cart is empty.</p>
           </span>
           <div className='p-4'>
+            <Link to='/'>
             <button type="button" className="dark:text-white focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Keep Shopping</button>
+            </Link>
           </div>
 
         </center>
